d1c8d02e5da401b6d31a2d2d87aac8f98a439fd6
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-04-14 23:01:50 +0300

    Some fix

--- src/main/java/com/esmods/cpapi/Launcher.java
+++ src/main/java/com/esmods/cpapi/Launcher.java
@@ -1,5 +1,18 @@
 package com.esmods.cpapi;
 
+import net.mcreator.plugin.JavaPlugin;
+import net.mcreator.plugin.Plugin;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class Launcher {
+public class Launcher extends JavaPlugin {
+
+	private static final Logger LOG = LogManager.getLogger("Colorful Powers API Plugin");
+
+	public Launcher(Plugin plugin) {
+		super(plugin);
+
+		LOG.info("Plugin was loaded");
+	}
+
 }
